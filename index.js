d56d7c3c3e07aafc73300b49619513f07ea8a5f9
Mark Karpeles <magicaltux@gmail.com>
2022-04-13 11:56:39 +0900

    fix var

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports.scanContract = function(code) {
 
 	// match our regexp
 	const matches = code.matchAll(/8063([0-9a-fA-F]{8})1461([0-9a-fA-F]{4})57/g);
-	for (const match of match) {
+	for (const match of matches) {
 		let f = match[1]; // keccak256 hash of function signature
 		if (f in signatures) {
 			f = signatures[f];
